92855f491f1360bd1ce0a7d4f91bca5f9914d209
coydog <coydog@devio.us>
2013-12-03 10:15:28 -0600

    Changed build script to build paper and l33t in separate directories. Removed resume.css. Using paper and l33t css again.

--- buildresume.sh
+++ buildresume.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
- cat htmlheader > index.html && Markdown.pl README.md >> index.html && cat htmlfooter >> index.html
+cp paper.css resume/resume.css
+cp l33t.css resume-l33t/resume.css
+ cat htmlheader > index.html \
+	 && Markdown.pl README.md >> index.html \
+	 && cat htmlfooter >> index.html \
+	 && cp index.html resume/index.html \
+	 && cp index.html resume-l33t/index.html
